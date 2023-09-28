293d7429607f848c1e3ac64dafd5599342f092e4
yauhenimalakhouski <122909276+yauhenimalakhouski@users.noreply.github.com>
2023-09-28 22:03:03 +0300

    menu loader added

--- src/components/Menu/container.jsx
+++ src/components/Menu/container.jsx
@@ -1,8 +1,17 @@
 import { fetchRestaurantDishs } from "@/services/api";
 
 import { Menu } from "./component";
+import { Suspense } from "react";
 
-export default async function MenuContainer({restaurantId}){
+export default function MenuContainer({restaurantId}){
+  return (
+    <Suspense fallback={<div>!!!!!!!!Loading...</div>}>
+      <MenuList restaurantId={restaurantId}/>
+    </Suspense>
+  )
+}
+
+export async function MenuList({restaurantId}){
   const dishes = await fetchRestaurantDishs(restaurantId);
   
   if (!dishes?.length) {
