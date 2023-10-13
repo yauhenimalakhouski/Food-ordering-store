8b8fd75019282a0af11173d772eb6c27e4813b7e
yauhenimalakhouski <122909276+yauhenimalakhouski@users.noreply.github.com>
2023-10-13 23:51:23 +0300

    empty cart updated

--- src/components/EmptyCart/component.jsx
+++ src/components/EmptyCart/component.jsx
@@ -2,13 +2,11 @@ import { LinkTo } from "../LinkTo/component";
 import styles from "./styles.module.css";
 
 export const EmptyCart = () => {
-    return <div>
-        <div>
-            <h1>Your cart is empty.</h1>
-            <h2>Choose your meals from the restaurants</h2>
-            <LinkTo to={`/restaurants`} text={"Show restaurants"}></LinkTo>
-            <h2>Or of all the dishes that are available</h2>
-            <LinkTo to={`/cart`} text={"Show all dishes"}></LinkTo>
-        </div>
+    return <div className={styles.root}>
+        <h1 className={styles.title}>Your cart is empty.</h1>
+        <h2 className={styles.secondary_title}>Choose your meals from the restaurants</h2>
+        <LinkTo to={`/restaurants`} text={"Show restaurants"}></LinkTo>
+        <h2 className={styles.secondary_title}>Or of all the dishes that are available</h2>
+        <LinkTo to={`/dishes`} text={"Show all dishes"}></LinkTo>
     </div>
 }
